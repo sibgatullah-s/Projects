74a4a7131c2ced47ae05f9d6eee02069a231cba2
sibgatullah-s <sshai106@uottawa.ca>
2019-02-09 00:14:37 -0500

    added date variable to store date

--- To Do List/Task.java
+++ To Do List/Task.java
@@ -1,5 +1,11 @@
 // class for a single task
+import java.util.Date;
 public class Task {
     private String message;
     private Date rightNow;
+
+    // constructor, takes in message
+    public Task (String message) {
+        rightNow = java.util.Calendar.getInstance().getTime();
+    }
 }
