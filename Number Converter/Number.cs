219096b443402a4f07e399ea990f8e5944ebb3cd
sibgatullah-s <sshai106@uottawa.ca>
2020-08-27 16:11:23 -0400

    basic framework

---
+++ Number Converter/Number.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Number {
+    // private
+    private float dec;
+    private string bin;
+    private string oct;
+    private string hex;
+
+    // public
+    public float Dec { get => dec; set => dec = value; }
+    public string Bin { get => bin; set => bin = value; }
+    public string Oct { get => oct; set => oct = value; }
+    public string Hex { get => hex; set => hex = value; }
+
+    public void updateDec() {
+
+    }
+
+    public void updateBin() {
+
+    }
+
+    public void updateOct() {
+
+    }
+
+    public void updateHex() {
+        
+    }
+}
---
+++ Number Converter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Number_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+}
